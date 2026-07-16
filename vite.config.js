05e3eac59c03fa10c042952dcf060649eb6094a3
Fernando Fernandes <fernando.fernandes@grupoantaris.com.br>
2026-07-16 18:23:38 -0400

    Fix Pages asset base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
+  base: "/N1MM_Cabrilo_to_ATLogger_File/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
