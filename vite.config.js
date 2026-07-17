670a7a7a752a4650cbae2df2e9ef6612a0539f8f
Fernando Fernandes <fernando.fernandes@grupoantaris.com.br>
2026-07-17 12:29:50 -0400

    Fix GitHub Pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
-  base: "/N1MM_Cabrillo_to_ATLogger_File/",
+  base: "./",
   build: {
     outDir: "dist",
     emptyOutDir: true,
